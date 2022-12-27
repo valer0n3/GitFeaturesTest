559b596c477a894f5971b24f67e290a002c8daf3
valer0n3 <vovave@gmail.com>
2022-12-27 11:56:14 +0300

    Initial commit

---
+++ src/main/java/git/feature/test/hackers/HackersApplication.java
@@ -0,0 +1,13 @@
+package git.feature.test.hackers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/feature/test/hackers/HackersApplicationTests.java
@@ -0,0 +1,13 @@
+package git.feature.test.hackers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
